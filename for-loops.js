6fab1ffb330be03bf96abed8653e84e151dc1e1c
preetikrp <preetikrp@gmail.com>
2017-04-11 15:59:23 -0500

    finished for loop

---
+++ for-loops.js
@@ -0,0 +1,61 @@
+for (var i=5; i<130; i+=10){
+	if (i===125){
+		i=i-5;
+	}
+	console.log(i);
+
+}
+
+
+
+for (var i=4096; i>1; i/=2){
+	var division=i / 2;
+
+	console.log(division);
+}
+
+var president =["George Washington", "John Adams",'Thomas Jefferson'];
+
+for (var i=0; i< president.length; i++){
+console.log("president #was" + president[i]);
+
+}
+
+for (var i= 5; i < 130; i+=10){
+    if(i===125){
+      i= i-5;
+    }
+    console.log(i);
+}
+
+for (var i= 4096; i>1; i/=2){
+	var newDivis= i / 2;
+	console.log(newDivis);
+
+}
+
+
+ var pres=["George Washington", "John Adams", "Thomas Jefferson"];
+for (var i=0; i < pres.length; i++){
+	
+	
+	console.log("President # was " + pres[i]);
+}
+
+
+var antSpecies = {
+  argentine: {},
+  army: {},
+  bigHeaded: {},
+  black: {},
+  bull: {},
+  carpenter: {},
+  crazy: {},
+  fire: {},
+  glider: {},
+  honeyPot: {},
+  jackJumper: {}
+}
+for (var emmet in antSpecies){
+    console.log(emmet);
+}
